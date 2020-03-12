4cdc2c974410f0cfeb3af59c87422cf96cb0ae6a
ianwow <ianwow@amazon.com>
2020-03-11 21:34:38 -0700

    fix invalid table query when invoking a GET on $WORKFLOW_API_ENDPOINT/workflow/execution/status/Error

--- source/workflowapi/app.py
+++ source/workflowapi/app.py
@@ -2380,7 +2380,19 @@ def list_workflow_executions_by_status(Status):
 
     workflow_executions = response['Items']
     while 'LastEvaluatedKey' in response:
-        response = table.query(ExclusiveStartKey=response['LastEvaluatedKey'])
+        response = table.query(
+            ExclusiveStartKey=response['LastEvaluatedKey'],
+            IndexName='WorkflowExecutionStatus',
+            ExpressionAttributeNames={
+                '#workflow_status': "Status",
+                '#workflow_name': "Name"
+            },
+            ExpressionAttributeValues={
+                ':workflow_status': Status
+            },
+            KeyConditionExpression='#workflow_status = :workflow_status',
+            ProjectionExpression = projection_expression
+        )
         workflow_executions.extend(response['Items'])
 
     return workflow_executions
@@ -2416,7 +2428,19 @@ def list_workflow_executions_by_assetid(AssetId):
 
     workflow_executions = response['Items']
     while 'LastEvaluatedKey' in response:
-        response = table.query(ExclusiveStartKey=response['LastEvaluatedKey'])
+        response = table.query(
+            ExclusiveStartKey=response['LastEvaluatedKey'],
+            IndexName='WorkflowExecutionAssetId',
+            ExpressionAttributeNames={
+                '#workflow_status': "Status",
+                '#workflow_name': "Name"
+            },
+            ExpressionAttributeValues={
+                ':assetid': AssetId
+            },
+            KeyConditionExpression='AssetId = :assetid',
+            ProjectionExpression = projection_expression
+        )
         workflow_executions.extend(response['Items'])
 
     return workflow_executions
